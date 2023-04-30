066743cb36ea229705c210678cd560967bdc5c31
Xkrilandar <44369165+Xkrilandar@users.noreply.github.com>
2023-04-30 15:36:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+new fullpage('#fullpage', {
+    //options here
+    //autoScrolling:true,
+    scrollHorizontally: true,
+    sectionsColor : ['#ae2d06', '#b58b57', '#466f57', '#d11c06', '#466f57'],
+    navigation : true,
+    slidesNavigation : true
+});
