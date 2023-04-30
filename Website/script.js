d2ee45223690e367c3047638ebe85135c3769398
Xkrilandar <44369165+Xkrilandar@users.noreply.github.com>
2023-04-30 15:43:31 +0200

    Delete Website directory

--- Website/script.js
+++
@@ -1,8 +0,0 @@
-new fullpage('#fullpage', {
-    //options here
-    //autoScrolling:true,
-    scrollHorizontally: true,
-    sectionsColor : ['#ae2d06', '#b58b57', '#466f57', '#d11c06', '#466f57'],
-    navigation : true,
-    slidesNavigation : true
-});
