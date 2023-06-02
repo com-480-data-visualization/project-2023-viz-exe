53c900625c42f2f1269fdf54fd961b850dee91ea
emmadeno <35627867+emmadeno@users.noreply.github.com>
2023-06-02 13:59:38 +0200

    test again

--- docs/script.js
+++ docs/script.js
@@ -5,5 +5,5 @@ new fullpage('#fullpage', {
     scrollHorizontally: true,
     sectionsColor : ['#ae2d06', '#b58b57', '#466f57', '#d11c06', '#466f57'],
     navigation : true,
-    slidesNavigation : true
+    slidesNavigation : true,
 });
