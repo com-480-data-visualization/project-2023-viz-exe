51076fb2dc98f86b35851bb8b2b317d362480c53
emmadeno <35627867+emmadeno@users.noreply.github.com>
2023-06-02 13:56:24 +0200

    test

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,7 @@
 new fullpage('#fullpage', {
     //options here
     //autoScrolling:true,
+    licenseKey: 'gplv3-license',
     scrollHorizontally: true,
     sectionsColor : ['#ae2d06', '#b58b57', '#466f57', '#d11c06', '#466f57'],
     navigation : true,
