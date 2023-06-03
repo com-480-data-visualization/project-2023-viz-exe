555335a3de9bd8c92d1be203d8a16daaed63c03e
Xavier Theimer-Lienhard <xavier.theimer-lienhard@epfl>
2023-06-03 18:39:22 +0200

    c joli

--- docs/script.js
+++ docs/script.js
@@ -3,7 +3,7 @@ new fullpage('#fullpage', {
     //autoScrolling:true,
     licenseKey: 'gplv3-license',
     scrollHorizontally: true,
-    sectionsColor : ['#ae2d06', '#ae2d06', '#466f57', '#b58b57', '#466f57'],
+    sectionsColor : ['#ae2d06', '#b58b57', '#466f57', '#b58b57', '#466f57'],
     navigation : true,
     slidesNavigation : true,
 });
